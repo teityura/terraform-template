4a84af8a374aeef2a8bce30719d0c75d747593c8
teityura <teityura@gmail.com>
2025-09-21 10:21:57 +0000

    add data volume

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.14.0"
   required_providers {
     openstack = {
-      source  = "terraform-provider-openstack/openstack"
+      source = "terraform-provider-openstack/openstack"
       version = "~> 1.53.0"
     }
   }
@@ -11,16 +11,16 @@ terraform {
 provider "openstack" {}
 
 resource "openstack_blockstorage_volume_v3" "main" {
-  name     = "${var.server_config.name}-root"
-  size     = var.server_config.volume_size
+  name = "${var.server_config.name}-root"
+  size = var.server_config.volume_size
   image_id = var.server_config.image_id
   enable_online_resize = true
 }
 
 resource "openstack_compute_instance_v2" "main" {
-  name            = var.server_config.name
-  flavor_id       = var.server_config.flavor_id
-  key_pair        = var.server_config.key_pair
+  name = var.server_config.name
+  flavor_id = var.server_config.flavor_id
+  key_pair = var.server_config.key_pair
   security_groups = var.server_config.security_groups
 
   network {
@@ -28,16 +28,16 @@ resource "openstack_compute_instance_v2" "main" {
   }
 
   block_device {
-    uuid                  = openstack_blockstorage_volume_v3.main.id
-    source_type           = "volume"
-    destination_type      = "volume"
-    boot_index            = 0
+    uuid = openstack_blockstorage_volume_v3.main.id
+    source_type = "volume"
+    destination_type = "volume"
+    boot_index = 0
     delete_on_termination = true
   }
 }
 
 resource "openstack_networking_floatingip_v2" "main" {
-  pool  = var.server_config.net_ext
+  pool = var.server_config.net_ext
 }
 
 resource "openstack_compute_floatingip_associate_v2" "main" {
--- terraform/ssh.tf
+++ terraform/ssh.tf
@@ -6,12 +6,12 @@ module "ssh_config" {
   source = "git::https://github.com/teityura/terraform-modules.git//ssh-config"
 
   server_details = local.server_details
-  project_name   = local.project_name
+  project_name = local.project_name
   ssh_user = local.config.ssh_user
   ssh_key_path = local.config.ssh_key_path
 }
 
 output "ssh_config_path" {
   description = "SSH config file"
-  value       = module.ssh_config.filename
+  value = module.ssh_config.filename
 }
